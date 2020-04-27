1d1c6e2727c97ca19d304f37c35d53297315e845
Zackwang07 <61254034+Zackwang07@users.noreply.github.com>
2020-04-27 16:43:32 -0600

    react-01

--- react-01/src/App.js
+++ react-01/src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MyComponent from './components/MyComponent.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.counter = 21;
+    this.state = {
+      myState: "TBD"
+    };
+  }
+
+  onPushMe = () =>{
+    console.log("you pushed me");
+    this.counter ++;
+    console.log(this.counter);
+    this.setState({
+      myState: "now:" + this.counter
+    });
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>I am in control of this application and my name is Zack {this.counter} {this.state.myState}</h1>
+          <button onClick={this.onPushMe}>Push Me</button>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <MyComponent whatToSay = {this.onPushMe}/>
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
