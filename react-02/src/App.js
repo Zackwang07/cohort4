6986f321f6816cf65a42d1b86e4bfe1e8103ca25
Zackwang07 <61254034+Zackwang07@users.noreply.github.com>
2020-05-07 20:54:20 -0600

    game

--- react-02/src/App.js
+++ react-02/src/App.js
@@ -1,38 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import SuperMarioSVG from './components/superMario';
 import NarutoSVG from './components/naruto';
 import IronManSVG from './components/ironMan';
 import DarthVaderSVG from './components/darthVader';
+import Starter from './components/starter';
+import Game from './components/ticTacToe';
 import './App.css';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      display: "home",
+    }
+  }
+
+  chooseApp=(e)=>{
+    let chosenApp = e.target.id;
+    
+    this.setState({
+      display: chosenApp
+    })
+    
+  }
+  showApp = () => {
+    
+    if (this.state.display === "iconSuperMario"){
+      return <Starter/>
+    }
+    if (this.state.display === "iconNaruto"){
+      return <Game/>
+    }
+  }
 
 
   render() {
     return (
       <div className="App" >
-        <div className="iconBox" >
-          <SuperMarioSVG id="iconSuperMario" className="icons"/>
-          <NarutoSVG id="iconNaruto" className="icons"/>
-          <IronManSVG id="iconIronMan" className="icons"/>
+        <div className="iconBox" onClick={this.chooseApp}>
+          <SuperMarioSVG id="iconSuperMario" className="icons" />
+          <NarutoSVG id="iconNaruto" className="icons" />
+          <IronManSVG id="iconIronMan" className="icons" />
           <DarthVaderSVG id="icondarthVader" className="icons" />
         </div>
 
 
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {this.showApp()}
         </header>
       </div>
     );
