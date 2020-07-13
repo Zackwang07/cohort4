b215729d3a5f4e0cda1cf9507168906f2a38e059
Zackwang07 <61254034+Zackwang07@users.noreply.github.com>
2020-07-13 17:30:14 -0600

    read a file

--- python/comp220/syntax_reader.py
+++ python/comp220/syntax_reader.py
@@ -1,23 +1,31 @@
-f = open("/Users/hui/Desktop/code/cohort4/01-getting-started/src/scripts/syntax.js", "r")
-lines = 0
-elses = 0
-characters = 0
-for line in f:
 
-    lines += 1
+file = "/Users/hui/Desktop/code/cohort4/01-getting-started/src/scripts/syntax.js"
+with open(file, "r") as f:
+    f_content = f.read()
+# lines = 0
+# elses = 0
+# characters = 0
+# for line in f:
 
-    characters += len(line)
+#     lines += 1
+
+#     characters += len(line)
     
-    line = line.strip()
-    words = line.split(" ")
-    for word in words:
-        if word == "else":
-            elses += 1
+#     line = line.strip()
+#     words = line.split(" ")
+#     for word in words:
+#         if word == "else":
+#             elses += 1
+
+lines = len(open(file, 'r').readlines())
+elses = f_content.count("else")
+characters = len(f_content)
 
 
-f.close()
+
 
 print("Total lines in Syntax.js: ", lines)
 print("Total 'else' in Syntax.js: ", elses)
 print("Total characters in Syntax.js: ", characters)
 
+f.close()
